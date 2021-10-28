e133623157e142aa7840eec1f16352eb1818cfe0
ravi sondagar <ravisondagar.codexive@gmail.com>
2021-10-28 10:25:35 +0530

    create datascience page

---
+++ src/pages/datascience/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function index() {
+    return (
+        <div className="bgImageDataScience">
+            <div className="dataScienceMainTitle">
+                <h1>데이터 사이언스</h1>
+            </div>
+        </div>
+    )
+}
+
+export default index
